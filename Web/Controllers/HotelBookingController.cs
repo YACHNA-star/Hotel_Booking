835738c18f35f989d36eec4e1732d30779577cb3
Lovelish <Lovelish@DESKTOP-UGGF7OP>
2024-05-19 23:39:31 +0530

    Add project files.

---
+++ Web/Controllers/HotelBookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+using System.Reflection;
+using Web.Models;
+namespace Web.Controllers;
+
+public class HotelBookingController : Controller
+{
+    private static List<HotelBooking> D = new List<HotelBooking>();
+    private static int i=0;
+    public IActionResult Index()   
+    {
+        return View(D);
+        //return View();// return a view that has same name with controller
+    }
+    public IActionResult Create()
+    {
+        var DV = new HotelBooking();
+        return View(DV);// return a view that has same name with controller
+    } 
+    public IActionResult CreateDetail(HotelBooking ViewModel)
+    {
+        ++i;
+        ViewModel.APP_ID = i;
+        //return View("Index");// return a view that has same name with controller
+        D.Add(ViewModel);
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Web/Models/HotelBooking.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web.Models
+{
+    public class HotelBooking
+    {
+
+        public int Id { get; set; }
+        public int APP_ID { get; set; }
+        public string? Name { get; set; }
+        
+        public DateOnly DateStart { get; set; }
+
+        public DateOnly DateEnd { get; set; }
+
+    }
+}
